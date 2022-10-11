04aa665dac36f69b432d436610f6b5b6a5839a62
Frederic Feliciane <fredericfeliciane8@gmail.com>
2022-10-11 13:32:46 +0400

    validation done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :material
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :email, presence: true, format: { with: /^(.+)@(.+)\.(.+)$/, message: "Email invalid" }, uniqueness: { case_sensitive: false }
+  # validates :encrypted_password, uniqueness: true
+
 end
