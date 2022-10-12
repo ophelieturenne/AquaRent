fb3b870d8e52068e8baa50262dc9f8ad6dbf0542
ophelie turenne <turenneophelie@gmail.com>
2022-10-12 16:46:46 +0400

    shit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :bookings, dependent: :destroy
   # validates :email, presence: true, format: { with: /^(.+)@(.+)\.(.+)$/, message: "Email invalid" }, uniqueness: { case_sensitive: false }
   # validates :encrypted_password, uniqueness: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require "open-uri"
+
+puts "starting seeding"
+
 puts "cleaning database"
 
-User.destroy_all
-Material.destroy_all
-Category.destroy_all
 Booking.destroy_all
+Material.destroy_all
+<<<<<<< HEAD
+User.destroy_all
+Category.destroy_all
+
+=======
+Category.destroy_all
+User.destroy_all
+>>>>>>> 8bbf6ea4138913ed0b58ffb753e42930dd7fb39f
 
 
 puts "creating categories"
@@ -28,9 +38,39 @@ category3.save
 
 
 puts "creating materials"
-# materials
-material1 = Material.create(name: "small paddle", image: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", price: 6.9, category: category1)
+
+file = URI.open("https://res.cloudinary.com/dvp1v2dej/image/upload/v1665561920/aquarent/canoe_zooayz.jpg")
+material = Material.new(name: "Canoe", price: 600, category: category1)
+material.photo.attach(io: file, filename: "canoe.png", content_type: "image/png")
+material.save
+
+
+file = URI.open("https://res.cloudinary.com/dvp1v2dej/image/upload/v1665561894/aquarent/fly_board_ebz8wv.jpg")
+material1 = Material.new(name: "Fly board", price: 600, category: category1)
+material1.photo.attach(io: file, filename: "fly_board.png", content_type: "image/png")
 material1.save
+
+file = URI.open("https://res.cloudinary.com/dvp1v2dej/image/upload/v1665561890/aquarent/kayak_f8wwpj.jpg")
+material2 = Material.new(name: "Kayak", price: 600, category: category1)
+material2.photo.attach(io: file, filename: "kayak.png", content_type: "image/png")
+material2.save
+
+file = URI.open("https://res.cloudinary.com/dvp1v2dej/image/upload/v1665561890/aquarent/surf_board_fndqce.jpg")
+material3 = Material.new(name: "Surf Board", price: 600, category: category1)
+material3.photo.attach(io: file, filename: "surf.png", content_type: "image/png")
+material3.save
+
+
+file = URI.open("https://res.cloudinary.com/dvp1v2dej/image/upload/v1665561890/aquarent/wind_foiling_g2ubyi.jpg")
+material4 = Material.new(name: "wind foiling", price: 600, category: category3)
+material4.photo.attach(io: file, filename: "wind_foiling.png", content_type: "image/png")
+material4.save
+
+file = URI.open("https://res.cloudinary.com/dvp1v2dej/image/upload/v1665561890/aquarent/fishing_rod_xopehu.jpg")
+material5 = Material.new(name: "fishing Rod", price: 600, category: category3)
+material5.photo.attach(io: file, filename: "fishing_rod.png", content_type: "image/png")
+material5.save
+
 
 puts "creating users"
 user1 = User.new(email: "john@gmail.com", password: "123456")
