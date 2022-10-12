43498c27b3e40f98a83408bcb63741d015fff133
ophelie turenne <turenneophelie@gmail.com>
2022-10-12 10:43:08 +0400

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: %i[index destroy]
   devise_for :users
-  # get '/materials', to: 'materials#index'
-  # get '/materials/:id/', to: 'bookings#new'
-  # post '/materials/:id/', to: 'bookings#new'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
