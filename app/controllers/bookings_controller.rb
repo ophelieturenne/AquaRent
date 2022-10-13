bcf8fb8dbc1f02705132baf1381acdb08cc315fe
ophelie turenne <turenneophelie@gmail.com>
2022-10-13 14:18:46 +0400

    added user in boonking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @bookings = Booking.all
+    @bookings = Booking.where(user: current_user)
   end
 
   def new
