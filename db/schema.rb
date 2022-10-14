6d915015a1b71e81f834f67668ad56df86001787
ophelie turenne <turenneophelie@gmail.com>
2022-10-14 10:07:22 +0400

    updated booking and material table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_045636) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_060452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_045636) do
     t.date "start_date"
     t.date "end_date"
     t.bigint "material_id", null: false
-    t.float "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["material_id"], name: "index_bookings_on_material_id"
@@ -66,7 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_045636) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
     t.index ["category_id"], name: "index_materials_on_category_id"
   end
 
